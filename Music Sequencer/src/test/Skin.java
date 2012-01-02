5a8e27b14cb915d7ca57909dece565245cb4f894
Nanyou <guann@bxscience.edu>
2012-01-01 21:53:37 -0500

    Forgot to add skin class

---
+++ Music Sequencer/src/test/Skin.java
@@ -0,0 +1,17 @@
+package test;
+
+import java.awt.Graphics;
+
+import javax.swing.JPanel;
+
+public class Skin extends JPanel {
+	
+	public Skin() {
+		setSize(800, 600);
+	}
+	
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+	}
+
+}
